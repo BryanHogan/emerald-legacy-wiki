8e544977ae9e330b4541dab822d318eb72872da4
BryanHogan <bryanhoganme@gmail.com>
2025-02-24 00:36:40 +0100

    git init pokemon emerald legacy wiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,45 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Emerald Legacy',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			  ],
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/cRz-Shadows/Pokemon_Emerald_Legacy',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Intro',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Welcome', slug: 'general/welcome' },
+						{ label: 'How To Play', slug: 'general/how-to-play' },
+					],
+				},
+				{
+					label: 'General Changes',
+					items: [
+						{ label: '👷‍♂️ Trainers', slug: 'general-changes/trainers' },
+						{ label: '💰 Items', slug: 'general-changes/items' },
+						{ label: '⚔️ Move Changes', slug: 'general-changes/move-changes' },
+					],
+				},
+				{
+					label: 'Pokemon Changes',
+					items: [
+						{ label: 'Overview', slug: 'pokemon-changes/overview' },
+						{ label: '📊 Stat Changes', slug: 'pokemon-changes/stats' },
+						{ label: '🦖 Evolution Changes', slug: 'pokemon-changes/evolutions' },
+						{ label: '💿 Learnset Changes', slug: 'pokemon-changes/learnsets' },
+						{ label: '☀️ Ability Changes', slug: 'pokemon-changes/abilities' },
+					],
+				},
+				{
+					label: 'Miscellaneous',
+					items: [
+						{ label: 'About', slug: 'miscellaneous/about' },
 					],
 				},
 				{
